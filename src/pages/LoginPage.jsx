b818225933179ecfdcb63e42c479f07d9f44aaf1
ganangadjie <ganangaji221@gmail.com>
2023-11-02 00:38:12 +0700

    feat(login): fixing LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../assets/css/Login.css";
-import { APIAuth } from "../config/APIs/Auth";
+import { APIAuth } from "../apis/APIAuth";
 import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
